d455017c183f74fdb2da763d4b65bdd213547d85
sjoelee <sammy.joe.lee@gmail.com>
2014-04-06 17:21:31 -0700

    Cleaned up code

--- cf_v3.py
+++ cf_v3.py
@@ -11,24 +11,52 @@ import nltk as nltk
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from nltk import NaiveBayesClassifier
 from nltk.tokenize import RegexpTokenizer
-from nltk.probability import ProbDistI
+from nltk.probability import ProbDistI, DictionaryProbDist
 
 NUM_WEATHER = 5
-
-class ProbDist(dict):
-    # def __init__(self, prob_vals, prob_labels):
-    #     dir.__init__()
-    #     self.labels = prob_labels
-    #     self.vals = prob_vals
-    def __init__(self, *args, **kw):
-        super(ProbDist,self).__init__(*args, **kw)
-        self.itemlist = super(ProbDist,self).keys()
+class LabelProbD(ProbDistI):
+    def __init__(self, prob_dict, labels):
+        self._prob_dict = prob_dict
+        self._labels = labels
 
     def prob(self, sample):
-        return self[sample]
+        return self._prob_dist[sample]
+
+    def max(self):
+        if not hasattr(self, '_max'):
+            self._max = max((p,v) for (v,p) in self._prob_dist.items())[1]
+        return self._max
 
     def samples(self):
-        return self.itemlist
+        return self._prob_dict.keys()
+
+class FeatureProbD(ProbDistI):
+    def __init__(self, prob_dist, feature_dict):
+        self._prob_dist = prob_dist
+        self._feature_dict = feature_dict.copy()
+
+    def prob(self, sample):
+        if sample in self._feature_dict.keys():
+            idx = self._feature_dict[sample]
+            return self.prob_dist(idx)
+        return -1
+
+    def logprob(self, sample):
+        # Default definition, in terms of prob()
+        p = self.prob(sample)
+        if p == 0:
+            # Use some approximation to infinity.  What this does
+            # depends on your system's float implementation.
+            return _NINF
+        else:
+            return math.log(p, 2)
+
+    def max(self):
+        max_idx = np.argmax(self.prob_dist)
+        return [feature for feature, idx in vocab if idx==max_idx]
+
+    def samples(self):
+        return feature_dict.keys() #hack, returns the whole dictionary
 
 def tokenize_tweet(data):
     tokenizer = RegexpTokenizer('\w+')
@@ -52,6 +80,11 @@ def sanitize_tweets(train_tweet):
     return clean_tweets
 
 def get_vocab(clean_tweets):
+    """
+    Similar to TfidfVectorizer, CountVectorizer removes all the common words
+    based on word occurrences in the corpus.
+    Returns a sorted vocab list of words.
+    """
     cv = CountVectorizer(max_features = 1000,
                          stop_words = 'english',
                          lowercase = True,
@@ -86,7 +119,7 @@ def get_class_tweets_and_indices(train_set, class_vector, train_count_d):
 
 def main():    
     #
-    # Note: make sure you remove empty lines at the end of hte training set!
+    # Note: make sure you remove empty lines at the end of the training set!
     #
     paths = ['./train_short.csv', './test.csv']
     train = pd.read_csv(paths[0])
@@ -112,8 +145,7 @@ def main():
     classes_prob_dict = {}
     for idx, label in enumerate(labels):
         classes_prob_dict[label] = classes_prob[idx]
-    classes_prob = ProbDist(classes_prob_dict)
-    print classes_prob.prob('s5')
+    classes_prob = DictionaryProbDist(classes_prob_dict)
 
     clean_tweets = sanitize_tweets(train_tweet)
     train_set = clean_tweets[0:9]
@@ -151,6 +183,8 @@ def main():
 
     # Sum all the word counts for each class. This counts the number of times a
     # feature has occurred in the respective classes
+    # NOTE: This seems incredibly inefficient. Why create a 2D matrix that can
+    # be potentially HUGE?
     train_count_class = np.zeros(shape=(NUM_WEATHER, len(vocab)))
     train_count_class[0][:] = np.sum(train_count_s1, axis=0)
     train_count_class[1][:] = np.sum(train_count_s2, axis=0)
@@ -163,20 +197,47 @@ def main():
     # represents a feature instance X_i=x.
     # The keys are (label, fname). From our count_vocab, we'll need to extract
     # the fname and its corresponding index.
-    feat_prob_dist = {}
-    for class_val in range(0,NUM_WEATHER):
+    # feat_prob_dist = {}
+    # test_dict = {}
+    # for class_val in range(0,NUM_WEATHER):
+    #     for feature, idx in vocab:
+    #         feat_prob_dist[class_val, feature] =\
+    #             train_count_class[class_val][idx]/float(classes_size[class_val])
+    feature_dict = {}
+    for feature, idx in vocab:
+        feature_dict[feature] = idx
+
+    feat_prob_dist = np.zeros(shape=(NUM_WEATHER, len(vocab)))
+    for class_val in range(0, NUM_WEATHER):
         for feature, idx in vocab:
-            feat_prob_dist[class_val, feature] =\
-                train_count_class[class_val][idx]/float(classes_size[class_val])
-    feat_prob_dist = ProbDist(feat_prob_dist)
-    print len(feat_prob_dist.keys())
+            feat_prob_dist[class_val][idx] = train_count_class[class_val][idx]/\
+                float(classes_size[class_val])
+
+    # For class 0
+    classProbDist = []
+    classProbDist.append(FeatureProbD(feat_prob_dist[0][:], feature_dict))
+    classProbDist.append(FeatureProbD(feat_prob_dist[1][:], feature_dict))
+
+    print classProbDist[0].prob('sun')
+              
+    # for class_val in range(0, NUM_WEATHER):
+    #     for word in clean_tweets[7].lower().split(' '):
+    #         if (class_val, word) in feat_prob_dist.keys():
+    #             test_dict[class_val, word] =\
+    #                 feat_prob_dist[class_val, word]
+    #         else: 
+    #             test_dict[class_val, word] = 0
 
     # Create a NaiveBayesClassifier object based off of our probability
     # distributions
     nb = NaiveBayesClassifier(classes_prob, feat_prob_dist)
-    nb.classify([clean_tweets[8]])
-    # Test
+    # nb.classify(test_dict)
 
+    # Self implementation of NB
+
+    test_prob = np.zeros((5,1))
+    
+    print test_prob
     # Form the conditional probability of each feature/word given a certain class
     # Transform count to a 2-D matrix of conditional probabilities for each class. 
     # print vocab
